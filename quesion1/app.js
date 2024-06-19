e1f9d19196660fe63b6e690be7d3b1c5c68fd747
pujari saidatthu <pujarisaidattu6@gmail.com>
2024-06-19 11:34:45 +0530

    updating windows

--- quesion1/app.js
+++ quesion1/app.js
@@ -11,3 +11,29 @@ const THIRD_PARTY_URLS = {
   'e': 'http://20.244.56.144/test/even',
   'r': 'http://20.244.56.144/test/rand'
 };
+let window = [];
+
+const fetchNumbers = async (url) => {
+  try {
+    const response = await axios.get(url, { timeout: 500 });
+    if (response.status === 200) {
+      const data = response.data;
+      const numbersKey = Object.keys(data)[0];
+      return data[numbersKey];
+    }
+  } catch (error) {
+    console.error('Error fetching numbers:', error.message);
+    return [];
+  }
+};
+const updateWindow = (newNumbers) => {
+    const uniqueNumbers = [...new Set(newNumbers)];
+    uniqueNumbers.forEach(num => {
+      if (!window.includes(num)) {
+        if (window.length >= WINDOW_SIZE) {
+          window.shift();
+        }
+        window.push(num);
+      }
+    });
+  };
